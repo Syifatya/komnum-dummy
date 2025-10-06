76a7ebd3477f456b7fc787c5f96df379ce98f34f
suciwrnd <suciwrnd@gmail.com>
2025-10-06 10:14:34 +0700

    Add files via upload

---
+++ tabel.py
@@ -0,0 +1,15 @@
+def f(x):
+    return x*3-3*x-7
+
+def batas_atas()
+
+def tabel(a, b, n, h):
+    a = int(input("Masukkan batas atas = "))
+    b = int(input("Masukkan batas bawah = "))
+    n = int(input("Masukkan jumlah iterasi = "))
+    h = (a-b)/n
+
+    for i in range:
+        x = 
+    
+    
