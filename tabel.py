544490d82159d12460697d9cadf38702860984bc
Iftitah Putri Syahna <24iftitah@mhs.usk.ac.id>
2025-10-06 11:37:30 +0700

    Update tabel.py

--- tabel.py
+++ tabel.py
@@ -1,21 +1,28 @@
 def f(x):
-    return x*3-3*x-7
+    return x**3-2*x-7
 
-# def batas_atas()
+a = float(input("Masukkan batas atas = "))
+b = float(input("Masukkan batas bawah = "))
+n = float(input("Masukkan jumlah iterasi = "))
 
 def tabel():
-    a = int(input("Masukkan batas atas = "))
-    b = int(input("Masukkan batas bawah = "))
-    n = int(input("Masukkan jumlah iterasi = "))
     h = (a-b)/n
 
-    for i in range(0, n+1):
-        xi = (b+(i*h))
-        y = f(xi)
-
-        print (f"{i}\t {xi:.6f}\t {y:.6f}")
+    for i in range(n):
+        x = (b+(i*h))
+        y = f(x[i])
+        if f(x[i]) == 0:
+            print(x[i])
+            return x[i]
+        
+        elif f(x[i])*f(x[i+1]) < 0:
+            if abs(f(x[i])) < abs(f(x[i+1])):
+                print(x[i])
+                return x[i]
+            else:
+                print(x[i+1])
+                return x[i+1]
+    print("\nTidak ditemukan perubahan tanda dalam interval tersebut.")
+    return None
+            
 tabel()
-
-
-    
-    
